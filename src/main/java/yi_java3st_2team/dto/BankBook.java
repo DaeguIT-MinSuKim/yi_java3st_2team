aa7b63133fabc43ffbd1ee4e518e276107821d78
Sunny Park <dlstjs8246@naver.com>
2020-02-20 15:36:53 +0900

    bankbook dto commit(하나씨 요청)

---
+++ src/main/java/yi_java3st_2team/dto/BankBook.java
@@ -0,0 +1,65 @@
+package yi_java3st_2team.dto;
+
+import java.util.Date;
+
+public class BankBook {
+	private String accountNum;
+	private Customer custCode;
+	private Plan accountPlanCode;
+	private Date accountOpenDate;
+	private float accountInterest;
+	private long accountBalance;
+	public BankBook() {
+		
+	}
+	public BankBook(Customer custCode) {
+		this.custCode = custCode;
+	}
+	
+	public BankBook(String accountNum, Customer custCode, Plan accountPlanCode, Date accountOpenDate,
+			float accountInterest, long accountBalance) {
+		this.accountNum = accountNum;
+		this.custCode = custCode;
+		this.accountPlanCode = accountPlanCode;
+		this.accountOpenDate = accountOpenDate;
+		this.accountInterest = accountInterest;
+		this.accountBalance = accountBalance;
+	}
+	public String getAccountNum() {
+		return accountNum;
+	}
+	public void setAccountNum(String accountNum) {
+		this.accountNum = accountNum;
+	}
+	public Customer getCustCode() {
+		return custCode;
+	}
+	public void setCustCode(Customer custCode) {
+		this.custCode = custCode;
+	}
+	public Plan getAccountPlanCode() {
+		return accountPlanCode;
+	}
+	public void setAccountPlanCode(Plan accountPlanCode) {
+		this.accountPlanCode = accountPlanCode;
+	}
+	public Date getAccountOpenDate() {
+		return accountOpenDate;
+	}
+	public void setAccountOpenDate(Date accountOpenDate) {
+		this.accountOpenDate = accountOpenDate;
+	}
+	public float getAccountInterest() {
+		return accountInterest;
+	}
+	public void setAccountInterest(float accountInterest) {
+		this.accountInterest = accountInterest;
+	}
+	public long getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(long accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	
+}
