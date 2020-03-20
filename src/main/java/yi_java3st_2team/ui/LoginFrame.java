a4a09573f2524053355a5be0f3dbb0deca32a8c8
Sunny Park <dlstjs8246@naver.com>
2020-03-20 21:20:11 +0900

    commit

--- src/main/java/yi_java3st_2team/ui/LoginFrame.java
+++ src/main/java/yi_java3st_2team/ui/LoginFrame.java
@@ -2,7 +2,6 @@ package yi_java3st_2team.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
