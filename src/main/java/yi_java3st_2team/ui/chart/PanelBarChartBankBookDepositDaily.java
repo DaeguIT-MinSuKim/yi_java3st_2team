86d0e2fe2c15420b632dba57e008647a49de28e2
Sunny Park <dlstjs8246@naver.com>
2020-03-22 15:14:07 +0900

    pull위한 commit

--- src/main/java/yi_java3st_2team/ui/chart/PanelBarChartBankBookDepositDaily.java
+++ src/main/java/yi_java3st_2team/ui/chart/PanelBarChartBankBookDepositDaily.java
@@ -1,14 +1,29 @@
 package yi_java3st_2team.ui.chart;
 
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import javax.swing.JOptionPane;
 
+import com.sun.javafx.charts.Legend;
+import com.sun.javafx.charts.Legend.LegendItem;
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.jmx.MXNodeAlgorithm;
+import com.sun.javafx.jmx.MXNodeAlgorithmContext;
+import com.sun.javafx.sg.prism.NGNode;
+
+import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Side;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
@@ -30,26 +45,30 @@ public class PanelBarChartBankBookDepositDaily extends JFXPanel implements InitS
 	@Override
 	public Scene createScene() {
 		Group root = new Group();
-		Scene scene = new Scene(root, Color.ALICEBLUE);
+		Scene scene = new Scene(root, Color.WHITE);
 		root.setAutoSizeChildren(true);
 		
 		//막 대형 차트의 X 축과 Y 축을 정의하고 레이블을 설정
 		CategoryAxis xAxis = new CategoryAxis();
+		xAxis.setTickLabelFont(new javafx.scene.text.Font(15));
+		xAxis.autosize();
 		xAxis.setLabel("고객");
 
 		NumberAxis yAxis = new NumberAxis();
 		yAxis.setLabel("거래량");
+		yAxis.autosize();
+		yAxis.setTickLabelFont(new javafx.scene.text.Font(15));
 
 		barChart = new BarChart<>(xAxis, yAxis);
 		barChart.setTitle("고객별 예금 일일 거래 내역 조회");
-		
-		barChart.setPrefSize(800, 600);
+		barChart.setLegendSide(Side.BOTTOM);
+		barChart.setPrefSize(1100, 500);
+		barChart.setStyle("-fx-font-size: " + 20 + "px;");
 		barChart.setData(getChartData());
 		root.getChildren().add(barChart);
 		return scene;
 	}
 	public XYChart.Series<String, Number> getChartData(AccountInfo info) {
-		JOptionPane.showMessageDialog(null, info);
 		XYChart.Series<String, Number> dataSeries = new Series<String, Number>();
 		dataSeries.getData().add(new XYChart.Data<>(info.getCustName(), info.getCount()));
 		return dataSeries;
@@ -74,7 +93,7 @@ public class PanelBarChartBankBookDepositDaily extends JFXPanel implements InitS
 			list3 = service.bankBookInfoDaily("김마바");
 			list4 = service.bankBookInfoDaily("김사아");
 			list5 = service.bankBookInfoDaily("김자차");
-			if(list1==null) {
+			if(list1.size()==0) {
 				accountInfo1 = new AccountInfo("김가나", 0);
 			}
 			else {
@@ -84,17 +103,45 @@ public class PanelBarChartBankBookDepositDaily extends JFXPanel implements InitS
 					}
 				}
 			}
-			if(list2==null) {
+			if(list2.size()==0) {
 				accountInfo2 = new AccountInfo("김다라", 0);
 			}
-			if(list3==null) {
+			else {
+				for(AccountInfo info : list2) {
+					if(info.getDiv().equals("예금")) {
+						accountInfo2 = new AccountInfo(info.getCustName(), info.getCount());
+					}
+				}
+			}
+			if(list3.size()==0) {
 				accountInfo3 = new AccountInfo("김마바", 0);
 			}
-			if(list4==null) {
+			else {
+				for(AccountInfo info : list3) {
+					if(info.getDiv().equals("예금")) {
+						accountInfo3 = new AccountInfo(info.getCustName(), info.getCount());
+					}
+				}
+			}
+			if(list4.size()==0) {
 				accountInfo4 = new AccountInfo("김사아", 0);
 			}
-			if(list5==null) {
+			else {
+				for(AccountInfo info : list4) {
+					if(info.getDiv().equals("예금")) {
+						accountInfo4 = new AccountInfo(info.getCustName(), info.getCount());
+					}
+				}
+			}
+			if(list5.size()==0) {
 				accountInfo5 = new AccountInfo("김자차", 0);
+			}
+			else {
+				for(AccountInfo info : list5) {
+					if(info.getDiv().equals("예금")) {
+						accountInfo5 = new AccountInfo(info.getCustName(), info.getCount());
+					}
+				}
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
